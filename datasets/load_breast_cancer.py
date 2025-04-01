911d8d759b0e2b944d0a5d975ee1429ca53ca9a6
bijay2052 <bijaypandey2000@gmail.com>
2025-04-01 09:49:03 +0545

    Added breast cancer datasets

--- datasets/load_breast_cancer.py
+++ datasets/load_breast_cancer.py
@@ -10,3 +10,24 @@ for attr in dir(breast_cancer):
     print('{attr}:'.format(attr=attr))
     print(getattr(breast_cancer, attr))
     print('\n\n')
+
+# Convert to Pandas DataFrame
+df = pd.DataFrame(breast_cancer.data, columns=breast_cancer.feature_names)
+
+# Add target variable (species) to the DataFrame
+df['target'] = breast_cancer.target
+
+# Class distribution (0 = benign, 1 = malignant)
+print(df['target'].value_counts())
+
+# Show dataset info
+print(df.head())
+print(df.info())
+
+# Summary statistics
+print(df.describe())
+
+# Visualize
+sns.histplot(df, bins=2, kde=True)
+# sns.pairplot(df, hue="target", diag_kind="kde")
+plt.show()
